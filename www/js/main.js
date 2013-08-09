40ca887b7a06fe060f97c5e406f64efc711c9d1a
Q-Team <maltamira@gmail.com>
2013-08-09 15:06:38 -0300

    Js for confirmation message

--- www/js/main.js
+++ www/js/main.js
@@ -1,1 +1,51 @@
+$(document).ready(function(){
+  $("#rsvp").hide();
+  
+  var height = $(window).height();
+  var width = $(window).width();
+  var xOffset = width/2 - $("#rsvp").width()/2;
+  var yOffset = height/2 - $("#rsvp").height()/2;
+  
+  $("#rsvp").css("left", xOffset + "px");
+  $("#rsvp").css("top", yOffset + "px");
+  
+  $("#rsvpYes").click(function(){
+	rsvp(true);
+	return false;
+  });
+  
+  $("#rsvpNo").click(function(){
+	rsvp(false);
+	return false;
+  });
+  
+  $(".rsvpConfirm").click(function(){
+	$(this).parent().fadeOut();
+	return false;
+  });
+  
+}); 
 
+window.onresize = function resize() { RePositionMsgBox(); }
+function RePositionMsgBox() {
+    var height = $(window).height();
+	var width = $(window).width();
+	var xOffset = width/2 - $("#rsvp").width()/2;
+	var yOffset = height/2 - $("#rsvp").height()/2;
+  
+	$("#rsvp").css("left", xOffset + "px");
+	$("#rsvp").css("top", yOffset + "px");
+}
+
+function rsvp(answer){
+	var msg = "";
+	
+	if(answer){
+		msg = "Are you sure you want to confirm your presence?";
+	} else {
+		msg = "Are you sure you are not coming?";
+	}
+	
+	$("#rsvpMessage").html(msg);
+	$("#rsvp").fadeIn();
+}
